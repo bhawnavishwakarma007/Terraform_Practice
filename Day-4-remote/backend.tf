b3aa7f31528308fa7ec4665ab875fa3cdab7f312
bhawna <bhawnavishwakarma464@gmail.com>
2026-01-03 17:15:36 +0530

    Day-4

---
+++ Day-4-remote/backend.tf
@@ -0,0 +1,23 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "how-to-train-ur-dragon"
+#     key    = "day-4/terraform.tfstate"
+#     region = "us-east-1"
+#     # Enable S3 native locking
+#     use_lockfile = true 
+#     # The dynamodb_table argument is no longer needed
+
+#   }
+# }
+
+terraform {
+  backend "s3" {
+    bucket = "how-to-train-ur-dragon"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    # Enable S3 native locking
+    use_lockfile = true 
+    # The dynamodb_table argument is no longer needed
+
+  }
+}
---
+++ Day-4-remote/main.tf
@@ -0,0 +1,5 @@
+
+resource "aws_s3_bucket" "first" {
+  bucket = "how-to-train-ur-dragonn"
+
+}
