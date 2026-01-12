06f5ce4d9d7292a9abbbb558386dde3d9336ed1a
bhawna <bhawnavishwakarma464@gmail.com>
2026-01-12 15:41:59 +0530

    Day-10-Datasource/Block

--- Day-10-DataSource/main.tf
+++ Day-10-DataSource/main.tf
@@ -12,4 +12,48 @@
 #     Name = "subnet"
 #   }
 # }
+data "aws_s3_bucket" "name" {
+  bucket = "how-to-train-ur-dragonnn"
+}
+
+resource "aws_s3_object" "name" {
+  bucket = data.aws_s3_bucket.name.id
+  key    = "app.zip"          #key = "app.py" for single file
+  source = "app.zip"          #source = "app.py"
+  etag   = filemd5("app.zip") #etag = filemd5("app.py")
+
+}
+resource "aws_iam_role" "name" {
+  name = "lambda-role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : ["sts:AssumeRole"],
+        "Principal" : {
+          "Service" : ["lambda.amazonaws.com"]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "name" {
+  role       = aws_iam_role.name.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "name" {
+  function_name = "lambda"
+  runtime       = "python3.12"
+  timeout       = 900
+  role          = aws_iam_role.name.arn
+  s3_bucket     = data.aws_s3_bucket.name.id  # S3 bucket where the Lambda deployment package (ZIP) is stored
+  s3_key        = aws_s3_object.name.key # S3 object key (ZIP file name) used by Lambda
+  memory_size      = 128
+  handler          = "app.lambda_handler"
+  source_code_hash = filebase64sha256("app.zip")
+
+}
 
