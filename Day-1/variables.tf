ebc097afecc5e8b3c2c8a1f426e7433f7cb4e87d
Bhawna Vishwakarma <159766023+bhawnavishwakarma007@users.noreply.github.com>
2026-01-25 18:03:24 +0530

    Update variables.tf

--- Day-1/variables.tf
+++ Day-1/variables.tf
@@ -1,11 +1,11 @@
 variable "ami_id" {
   description = "The AMI ID to use for the instance"
   type        = string
-  default     = ""
+  default     = "ami-068c0051b15cdb816"
 }
 
 variable "instance_type" {
   description = "The instance type to use"
   type        = string
-  default     = ""
+  default     = "t2.micro"
 }
